c70d1fef58e27dbda0d0d3330f06d89f5828beda
cy123 <974020129@qq.com>
2018-10-25 18:49:37 +0800

    gallery

---
+++ photo/data.js
@@ -0,0 +1,52 @@
+var data = [
+    {
+        img: '一出好戏' + '.jpg',
+        caption: '一出好戏',
+        desc: 'zen of css garden'
+    },
+    {
+        img: '江湖儿女' + '.jpg',
+        caption: '江湖儿女',
+        desc: 'zen of css garden'
+    },
+    {
+        img: '快把我哥带走' + '.jpg',
+        caption: '快把我哥带走',
+        desc: 'zen of css garden'
+    },
+    {
+        img: '动物世界' + '.jpg',
+        caption: '动物世界',
+        desc: 'zen of css garden'
+    },
+    {
+        img: '镰仓物语' + '.jpg',
+        caption: '镰仓物语',
+        desc: 'zen of css garden'
+    },
+    {
+        img: '黄蜂女现身' + '.jpg',
+        caption: '黄蜂女现身',
+        desc: 'zen of css garden'
+    },
+    {
+        img: '加尔维斯顿' + '.jpg',
+        caption: '加尔维斯顿',
+        desc: 'zen of css garden'
+    },
+    {
+        img: '阿浪的远方' + '.jpg',
+        caption: '阿浪的远方',
+        desc: 'zen of css garden'
+    },
+    {
+        img: '网络迷踪' + '.jpg',
+        caption: '网络谜踪',
+        desc: 'zen of css garden'
+    },
+    {
+        img: '明堂' + '.jpg',
+        caption: '明堂',
+        desc: 'zen of css garden'
+    }
+];
