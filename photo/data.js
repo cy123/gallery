ef5877979b320e352165fc987c0a8c36e826bb9d
cy123 <974020129@qq.com>
2018-10-25 19:41:37 +0800

    修改template

--- photo/data.js
+++ photo/data.js
@@ -2,51 +2,51 @@ var data = [
     {
         img: '一出好戏' + '.jpg',
         caption: '一出好戏',
-        desc: 'zen of css garden'
+        desc: 'the zen of css design'
     },
     {
         img: '江湖儿女' + '.jpg',
         caption: '江湖儿女',
-        desc: 'zen of css garden'
+        desc: 'the zen of css design'
     },
     {
         img: '快把我哥带走' + '.jpg',
         caption: '快把我哥带走',
-        desc: 'zen of css garden'
+        desc: 'the zen of css design'
     },
     {
         img: '动物世界' + '.jpg',
         caption: '动物世界',
-        desc: 'zen of css garden'
+        desc: 'the zen of css design'
     },
     {
         img: '镰仓物语' + '.jpg',
         caption: '镰仓物语',
-        desc: 'zen of css garden'
+        desc: 'the zen of css design'
     },
     {
         img: '黄蜂女现身' + '.jpg',
         caption: '黄蜂女现身',
-        desc: 'zen of css garden'
+        desc: 'the zen of css design'
     },
     {
         img: '加尔维斯顿' + '.jpg',
         caption: '加尔维斯顿',
-        desc: 'zen of css garden'
+        desc: 'the zen of css design'
     },
     {
         img: '阿浪的远方' + '.jpg',
         caption: '阿浪的远方',
-        desc: 'zen of css garden'
+        desc: 'the zen of css design'
     },
     {
         img: '网络迷踪' + '.jpg',
         caption: '网络谜踪',
-        desc: 'zen of css garden'
+        desc: 'the zen of css design'
     },
     {
         img: '明堂' + '.jpg',
         caption: '明堂',
-        desc: 'zen of css garden'
+        desc: 'the zen of css design'
     }
 ];
